04580e8f7d1c69d3bf4b71a41cf17d34dc388fcd
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-01-16 19:57:30 +0100

    Añadido SecurityFilterChain a SecurityConfig

--- src/main/java/init/config/security/SecurityConfig.java
+++ src/main/java/init/config/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package init.config.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import init.service.CustomUserDetailsManager;
 
@@ -29,4 +36,33 @@ public class SecurityConfig {
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	
+    	http
+    		
+    		.httpBasic(Customizer.withDefaults())
+    	
+    		.authorizeHttpRequests(authorize -> authorize
+						                .requestMatchers("/public/**").permitAll() 
+						                .anyRequest().authenticated())
+    		
+    		/*.cors(c -> {CorsConfigurationSource source = request -> {
+    			CorsConfiguration config = new CorsConfiguration();
+    			config.setAllowedOrigins(List.of("biblioteca-Enrique-Granados.es"));
+    			config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
+    			return config;
+    			};
+    			c.configurationSource(source);
+    		})*/
+    	
+    		.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    
+    
+    	
 }
---
+++ src/test/java/init/TestsCustomUserDetailsManager.java
@@ -0,0 +1,27 @@
+package init;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import init.dao.UsuariosDao;
+import init.service.CustomUserDetailsManager;
+import init.utilidades.Mapeador;
+
+@ExtendWith(MockitoExtension.class)
+public class TestsCustomUserDetailsManager {
+
+	@Mock
+	UsuariosDao usuariosDao;
+	
+	@Mock
+	Mapeador mapeador;
+	
+	@Mock
+	PasswordEncoder passwordEncoder;
+	
+	@InjectMocks
+	CustomUserDetailsManager customUserDetailsManager;
+}
