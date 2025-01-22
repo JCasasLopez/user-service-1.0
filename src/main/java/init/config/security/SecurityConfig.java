8ae44216929d51f9221789ad2f1fa814fde86663
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-01-22 19:28:34 +0100

    SecurityFilterChain modificado

--- src/main/java/init/config/security/SecurityConfig.java
+++ src/main/java/init/config/security/SecurityConfig.java
@@ -3,13 +3,16 @@ package init.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import init.config.security.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -18,10 +21,13 @@ public class SecurityConfig {
 
 	UserDetailsService userDetailsService;
 	PasswordEncoder passwordEncoder;
+	JwtAuthenticationFilter jwtAuthenticationFilter;
 	
-    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+    public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder,
+    												JwtAuthenticationFilter jwtAuthenticationFilter) {
 		this.userDetailsService = userDetailsService;
 		this.passwordEncoder = passwordEncoder;
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
 	}
 
     @Bean
@@ -33,22 +39,17 @@ public class SecurityConfig {
         return provider;
     }
     
-    /*@Bean
-    AuthenticationManager authManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
-    }*/
-    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.disable())  
-            /*.authenticationProvider(daoAuthenticationProvider(userDetailsService, passwordEncoder))*/ 
+            .sessionManagement(sessMang -> sessMang.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .authenticationProvider(daoAuthenticationProvider(userDetailsService, passwordEncoder))
             .authorizeHttpRequests(authorize -> authorize
             							.requestMatchers("/public/**").permitAll() 
             							.anyRequest().authenticated()
             )
-            .httpBasic(Customizer.withDefaults())
             .build();
     }
 	
