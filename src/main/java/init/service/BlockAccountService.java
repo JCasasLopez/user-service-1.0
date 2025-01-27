0d087f6934d03d2675c4ad3c5052c23b6e233d7f
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-01-27 12:11:09 +0100

    BlockAccountService creado

---
+++ src/main/java/init/service/BlockAccountService.java
@@ -0,0 +1,39 @@
+package init.service;
+
+import org.springframework.stereotype.Service;
+
+import init.dao.UsuariosDao;
+import init.entities.Usuario;
+
+@Service
+public class BlockAccountService {
+	
+	private static final int MAX_INTENTOS_FALLLIDOS = 3;
+	
+	UsuariosDao usuariosDao;
+	
+	public BlockAccountService(UsuariosDao usuariosDao) {
+		this.usuariosDao = usuariosDao;
+	}
+
+	public void incrementarIntentosFallidos(String username) {
+		Usuario usuario = usuariosDao.findByUsername(username);
+		int intentosFaliidos = usuario.getIntentosFallidos();
+		if(intentosFaliidos > MAX_INTENTOS_FALLLIDOS) {
+			bloquearCuenta(usuario);
+		}
+		usuario.setIntentosFallidos(intentosFaliidos+=1);
+		usuariosDao.save(usuario);
+	}
+	
+	public void resetearIntentosFallidos(String username) {
+		Usuario usuario = usuariosDao.findByUsername(username);
+		usuario.setIdUsuario(0);
+		usuariosDao.save(usuario);
+	}
+	
+	public void bloquearCuenta(Usuario usuario) {
+		usuario.setCuentaBloqueada(true);
+		usuariosDao.save(usuario);
+	}
+}
