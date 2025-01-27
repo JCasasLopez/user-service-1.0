63e2da6b5572d6e1e602f06cbab9636d6a47b4dd
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-01-27 15:12:15 +0100

    SecurityConfig modificado para incluir los handlers en el filtro para el login

--- src/main/java/init/config/security/SecurityConfig.java
+++ src/main/java/init/config/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package init.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,15 +22,21 @@ public class SecurityConfig {
 	UserDetailsService userDetailsService;
 	PasswordEncoder passwordEncoder;
 	JwtAuthenticationFilter jwtAuthenticationFilter;
+	CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+	CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 	
     public SecurityConfig(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder,
-    												JwtAuthenticationFilter jwtAuthenticationFilter) {
+			JwtAuthenticationFilter jwtAuthenticationFilter,
+			CustomAuthenticationFailureHandler customAuthenticationFailureHandler,
+			CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
 		this.userDetailsService = userDetailsService;
 		this.passwordEncoder = passwordEncoder;
 		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+		this.customAuthenticationFailureHandler = customAuthenticationFailureHandler;
+		this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
 	}
 
-    @Bean
+	@Bean
     DaoAuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService, 
     																	PasswordEncoder passwordEncoder) {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
@@ -42,11 +47,19 @@ public class SecurityConfig {
     
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	
+    	UsernamePasswordAuthenticationFilter loginFilter = new UsernamePasswordAuthenticationFilter();
+        //loginFilter.setAuthenticationManager(authenticationManager(http)); // Asigna el AuthenticationManager
+        loginFilter.setAuthenticationSuccessHandler(customAuthenticationSuccessHandler);       // Configura el SuccessHandler
+        loginFilter.setAuthenticationFailureHandler(customAuthenticationFailureHandler);       // Configura el FailureHandler
+        //loginFilter.setFilterProcessesUrl("/login");    
+        
+        // Endpoint para el login
         return http
             .csrf(csrf -> csrf.disable())
             .sessionManagement(sessMang -> sessMang.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            //Deshabilito LogoutFilter poque prefiero mi implementación personalizada
+            //Deshabilito LogoutFilter poque voy a usar mi implementación personalizada
             .logout(logout -> logout.disable()) 
             .authorizeHttpRequests(authorize -> authorize
             							.requestMatchers("public/**").permitAll() 
