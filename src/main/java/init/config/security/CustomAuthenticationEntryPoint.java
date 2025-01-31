e80e929c26143a2a1d2321909b14bc560bc5ca39
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-01-31 13:44:50 +0100

    CustomAuthenticationEntryPoint adaptado a StandardResponse

--- src/main/java/init/config/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/init/config/security/CustomAuthenticationEntryPoint.java
@@ -1,14 +1,21 @@
 package init.config.security;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import init.dao.UsuariosDao;
+import init.entities.StandardResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,9 +24,13 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
 	UsuariosDao usuariosdao;
+	ObjectMapper objectMapper;
 
 	public CustomAuthenticationEntryPoint(UsuariosDao usuariosdao) {
 		this.usuariosdao = usuariosdao;
+		this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 	}
 
 	@Override
@@ -29,34 +40,44 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 		response.setCharacterEncoding("UTF-8");
 		Object intentosRestantes = request.getAttribute("intentosRestantes");
 		int intentos = 0;
+		StandardResponse respuesta = null;
 		//En caso de que el problema no tenga nada que ver con un fallo de autenticación, 
 		//el valor de la variable intentosRestantes va a ser null, lo que provocará una 
 		//NullPointerException
 		if(intentosRestantes == null) {
+			respuesta = new StandardResponse (LocalDateTime.now(), 
+					"Error no relacionado con la autenticación", null, HttpStatus.INTERNAL_SERVER_ERROR);
 			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			response.getWriter().write("{\"error\": \"Ocurrió un error desconocido.\"}");
 			return;
 		} else {
 			intentos = (Integer) intentosRestantes;
 		}
 
 		if (authException instanceof LockedException) {
-			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-			response.getWriter().write("{\"error\": \"La cuenta está bloqueada. Contacte con soporte.\"}");
+			respuesta = new StandardResponse (LocalDateTime.now(), 
+					"La cuenta está bloqueada. Contacte con soporte", null, HttpStatus.UNAUTHORIZED);
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
 		} else if (authException instanceof BadCredentialsException) {
 			if(intentos >= 1) {
+				respuesta = new StandardResponse (LocalDateTime.now(), 
+						"Credenciales incorrectas. Le quedan " + intentosRestantes + " intentos", 
+						null, HttpStatus.UNAUTHORIZED);
 				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				response.getWriter().write("{\"error\": \"Credenciales incorrectas. Le quedan " 
-						+ intentosRestantes + " intentos\"}");
 			} else {
+				respuesta = new StandardResponse (LocalDateTime.now(), 
+						"Credenciales incorrectas. Su cuenta ha sido bloqueada por seguridad", 
+						null, HttpStatus.UNAUTHORIZED);
 				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				response.getWriter().write("{\"error\": \"Credenciales incorrectas. Su cuenta ha sido bloqueada\"}");
 			}
 
 		} else {
+			respuesta = new StandardResponse (LocalDateTime.now(), 
+										"Error desconocido", null, HttpStatus.INTERNAL_SERVER_ERROR);
 			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			response.getWriter().write("{\"error\": \"Ocurrió un error desconocido.\"}");
 		}
+		
+		String jsonResponse = objectMapper.writeValueAsString(respuesta);
+        response.getWriter().write(jsonResponse);
 	}
 }
