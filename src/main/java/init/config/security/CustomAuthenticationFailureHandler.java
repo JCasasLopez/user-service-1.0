80603ca62c8b5c067c07d688839bd8ca1afbc391
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-01-31 17:08:14 +0100

    Handlers adaptados a StandardResponse

--- src/main/java/init/config/security/CustomAuthenticationFailureHandler.java
+++ src/main/java/init/config/security/CustomAuthenticationFailureHandler.java
@@ -1,13 +1,20 @@
 package init.config.security;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import init.dao.UsuariosDao;
+import init.entities.StandardResponse;
 import init.entities.Usuario;
 import init.service.BlockAccountService;
 import init.utilidades.Constants;
@@ -21,12 +28,16 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 	BlockAccountService blockAccountService;
 	UsuariosDao usuariosDao; 
 	CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	ObjectMapper objectMapper;
 
 	public CustomAuthenticationFailureHandler(BlockAccountService blockAccountService, 
 			UsuariosDao usuariosDao, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
 		this.blockAccountService = blockAccountService;
 		this.usuariosDao = usuariosDao;
 		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+		this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 	}
 
 	@Override
@@ -38,10 +49,13 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 			// Aunque esta excepción debería manejarse en el CustomAuthenticationEntryPoint, 
 			//Spring Security no la propaga correctamente, así que se maneja directamente 
 			//en este punto para garantizar que el flujo funcione correctamente 
-			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-	        response.setContentType("application/json");
+			response.setContentType("application/json");
 	        response.setCharacterEncoding("UTF-8");
-	        response.getWriter().write("{\"error\": \"No existe el usuario " + username + "\"}");
+			StandardResponse respuesta = new StandardResponse (LocalDateTime.now(), 
+							"No existe el usuario " + username, null, HttpStatus.NOT_FOUND);
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			String jsonResponse = objectMapper.writeValueAsString(respuesta);
+	        response.getWriter().write(jsonResponse);
 	        return;
 		} else {
 			blockAccountService.incrementarIntentosFallidos(usuario);
--- src/main/java/init/config/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/init/config/security/CustomAuthenticationSuccessHandler.java
@@ -1,15 +1,19 @@
 package init.config.security;
 
 import java.io.IOException;
-import java.util.Map;
+import java.time.LocalDateTime;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import init.dao.UsuariosDao;
+import init.entities.StandardResponse;
 import init.entities.Usuario;
 import init.service.BlockAccountService;
 import init.service.JwtService;
@@ -23,22 +27,31 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 	BlockAccountService blockAccountService;
 	UsuariosDao usuariosDao;
 	JwtService jwtService;
+	ObjectMapper objectMapper;
 	
 	public CustomAuthenticationSuccessHandler(BlockAccountService blockAccountService, UsuariosDao usuariosDao,
 			JwtService jwtService) {
 		this.blockAccountService = blockAccountService;
 		this.usuariosDao = usuariosDao;
 		this.jwtService = jwtService;
+		this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 	}
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		String username = authentication.getName(); 
-        String token = jwtService.createToken();
+        String token = jwtService.createToken(); 
         response.setContentType("application/json");
-        Map<String, String> tokenResponse = Map.of("token", token);
-        new ObjectMapper().writeValue(response.getWriter(), tokenResponse);
+        response.setCharacterEncoding("UTF-8");
+		StandardResponse respuesta = new StandardResponse (LocalDateTime.now(), 
+						"Autenticación llevada a cabo con éxito", "Token: " + token, HttpStatus.OK);
+		response.setStatus(HttpServletResponse.SC_OK);
+		String jsonResponse = objectMapper.writeValueAsString(respuesta);
+        response.getWriter().write(jsonResponse);
+        
         Usuario usuario = usuariosDao.findByUsername(username);
 		blockAccountService.resetearIntentosFallidos(usuario);
 	}
