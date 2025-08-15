571871068db38f8d078ff87e6105f0d63ca0d9d6
Jorge Casas <j.casas.lopez.26@gmail.com>
2025-08-15 10:41:11 +0200

    Fix(Rol entity): add @Enumerated(EnumType.STRING) to nombreRol field

--- src/main/java/init/entities/Rol.java
+++ src/main/java/init/entities/Rol.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,6 +21,7 @@ public class Rol {
 	private int idRol;
 	
 	@Column(unique=true)
+	@Enumerated(EnumType.STRING)
 	private String nombreRol;
 	
 	@ManyToMany(mappedBy="roles")
